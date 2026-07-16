314bb675969fdc2d48aae0aa027321fb9e37b34d
daanniill <daniil.malakhov12@gmail.com>
2026-07-15 17:30:50 -0700

    overhaul test server, add option to specify port

--- tests/test_server.py
+++ tests/test_server.py
@@ -1,7 +1,17 @@
 import http.server
 import socketserver
+import argparse
 
-PORT = 9090
+# parser init
+parser = argparse.ArgumentParser(description="A script that opens up a simple http test server using TCP")
+
+# args
+parser.add_argument("-p", "--port", type=int, default=9001, help="Port on which server opens (default: 9001)")
+
+# parse the data
+args = parser.parse_args()
+
+PORT = args.port
 
 Handler = http.server.SimpleHTTPRequestHandler
 
