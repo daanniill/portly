acf1f31b2e900127b2baa9fbca949b78c748940a
daanniill <daniil.malakhov12@gmail.com>
2026-07-19 18:52:38 -0700

    created test function that tests the forwarder by creating a target server and a client server that sends a get request to the client through the forwarder

--- main_test.go
+++ main_test.go
@@ -1,12 +1,14 @@
 package main
 
 import (
+	"io"
 	"log"
 	"net"
 	"net/http"
 	"net/http/httptest"
 	"net/url"
 	"testing"
+	"time"
 )
 
 // startTestForwarder starts the forwarder on an automatically selected port.
@@ -50,7 +52,20 @@ func getTargetAddress(t *testing.T, serverURL string) string {
 	return parsedURL.Host
 }
 
+// creates a client to send requests
+func newTestHTTPClient() *http.Client {
+	return &http.Client{
+		Timeout: 3 * time.Second,
+		// disable keep alives that reuse tcp connections to send requests
+		// opens new tcp connection every time a client sends a request
+		Transport: &http.Transport{
+			DisableKeepAlives: true,
+		},
+	}
+}
+
 func TestForwarderForwardsHTTPResponse(t *testing.T) {
+	// create a target test server using httptest
 	targetServer := httptest.NewServer(
 		// defining a handler function for this test server to handle http request
 		// w is used to construct the HTTP response sent back to the client.
@@ -59,11 +74,47 @@ func TestForwarderForwardsHTTPResponse(t *testing.T) {
 			w.Header().Set("Connection", "close") // close tcp connections when response finishes
 			w.WriteHeader(http.StatusOK) // send 200 for succesful connections
 
-			if _, err := w.Write([]byte("concurrent response")); err != nil {
+			if _, err := w.Write([]byte("hello through forwarder")); err != nil {
 				t.Errorf("failed to write target response: %v", err)
 			}
 		}),
 	)
 
 	defer targetServer.Close()
+
+	targetAddress := getTargetAddress(t, targetServer.URL)
+	forwarderAddress := startTestForwarder(t, targetAddress)
+
+	// initialize a client to send http requests
+	client := newTestHTTPClient()
+
+	response, err := client.Get("http://" + forwarderAddress)
+	if err != nil {
+		t.Fatalf("request through forwarder failed: %v", err)
+	}
+	defer response.Body.Close()
+
+	body, err := io.ReadAll(response.Body)
+	if err != nil {
+		t.Fatalf("failed to read forwarded response: %v", err)
+	}
+
+	// ------------------- ACTUAL TESTS -------------------
+	if response.StatusCode != http.StatusOK {
+		t.Errorf(
+			"expected status %d, but got %d",
+			http.StatusOK,
+			response.StatusCode,
+		)
+	}
+
+	expectedBody := "hello through forwarder"
+
+	if string(body) != expectedBody {
+		t.Errorf(
+			"expected body %q, but got %q",
+			expectedBody,
+			string(body),
+		)
+	}
 }
