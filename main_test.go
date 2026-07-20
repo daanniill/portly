b97d6c645405072c5180a278fdb9c853e087f22a
daanniill <daniil.malakhov12@gmail.com>
2026-07-19 17:47:23 -0700

    create a function to start a test forwarder

--- main_test.go
+++ main_test.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"log"
+	"net"
 	"testing"
 )
 
@@ -8,11 +10,27 @@ import (
 //
 // Using port 0 tells the operating system to choose an available port,
 // which prevents test failures caused by ports already being occupied.
-func startTestForwardewr(t *testing.T, targetAddress string) string {
+func startTestForwarder(t *testing.T, targetAddress string) string {
 	t.Helper() // mark this function as a helper
 
-	listener, err := net.Listen("tcp", "127.0.0.1:0" // define a listener on any available port 
+	listener, err := net.Listen("tcp", "127.0.0.1:0")// define a listener on any available port 
 	if err != nil {
-		log.Fatalf("failed to listen on %s: %v", *localAddress, err)
-	} 
+		log.Fatalf("failed tocreate forwarder listener: %v", err)
+	}
+	
+	errCh := make(chan error, 1)
+
+	go func() {
+		errCh <- runForwarder(listener, targetAddress)
+	}()
+	
+	// free up resources after running tests
+	t.Cleanup(func() {
+		// check if listener closed succesfully
+		if err := listener.Close(); err != nil {
+			t.Errorf("failed to close listener: %v", err)
+		}
+	})
+
+	return listener.Addr().String() // returns address of where listener was opened on
 }
